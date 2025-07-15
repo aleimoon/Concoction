201afb7822cff2d1db1f4ebebf800f63f7ad2256
woofyboo <digim7459@gmail.com>
2025-07-15 23:38:22 +0300

    Cow Milking Gamerule

---
+++ src/main/java/net/mcreator/concoction/init/ConcoctionModGameRules.java
@@ -0,0 +1,21 @@
+
+/*
+ *	MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.concoction.init;
+
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+
+import net.minecraft.world.level.GameRules;
+
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
+public class ConcoctionModGameRules {
+	public static GameRules.Key<GameRules.IntegerValue> MILKING_INTERVAL;
+
+	@SubscribeEvent
+	public static void registerGameRules(FMLCommonSetupEvent event) {
+		MILKING_INTERVAL = GameRules.register("milkingInterval", GameRules.Category.MOBS, GameRules.IntegerValue.create(7200));
+	}
+}
